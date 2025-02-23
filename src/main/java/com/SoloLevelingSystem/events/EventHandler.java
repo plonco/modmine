c7050a9bdf8d1a3364067a4d323b8590969e8e21
Benjamin Zurita <benzurita1@gmail.com>
2025-02-23 16:17:01 -0300

    Update EventHandler.java

--- src/main/java/com/SoloLevelingSystem/events/EventHandler.java
+++ src/main/java/com/SoloLevelingSystem/events/EventHandler.java
@@ -1,35 +1,42 @@
 package com.SoloLevelingSystem.events;
 
+import com.SoloLevelingSystem.SoloLevelingSystem;
 import com.SoloLevelingSystem.configs.ConfigManager;
+import com.SoloLevelingSystem.attribute.ModAttributes;
+import com.SoloLevelingSystem.network.ModMessages;
+import com.SoloLevelingSystem.network.packets.SpawnParticlePacket;
 import com.SoloLevelingSystem.storage.EntityStorage;
-import com.SoloLevelingSystem.storage.LastAttackerStorage;
 import com.SoloLevelingSystem.storage.PlayerTargetStorage;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.ai.attributes.AttributeInstance;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
-import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
+import net.minecraftforge.common.capabilities.RegisterCapabilitiesEvent;
+import net.minecraftforge.event.AttachCapabilitiesEvent;
+import net.minecraftforge.event.ItemAttributeModifierEvent;
+import net.minecraftforge.event.ServerChatEvent;
+import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
-import net.minecraftforge.event.entity.player.AttackEntityEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.event.server.ServerStoppingEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.network.PacketDistributor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.level.ServerLevel;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
@@ -37,119 +44,103 @@ import java.util.UUID;
 public class EventHandler {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(EventHandler.class);
-    private static final double MAX_DISTANCE = 50.0D;
-    private static final Map<UUID, CompoundTag> preDeathEntityData = new HashMap<>(); // Temporary storage
+    private static final Map<UUID, CompoundTag> preDeathData = new HashMap<>();
 
     @SubscribeEvent
-    public static void onAttackEntity(AttackEntityEvent event) {
-        if (event.getTarget() instanceof LivingEntity) {
-            LivingEntity target = (LivingEntity) event.getTarget();
-            if (event.getEntity() instanceof Player) {
-                // Check if the target is a summoned entity
-                if (EntityStorage.isSummonedEntity(target.getUUID())) {
-                    event.setCanceled(true); // Cancel the attack
-                    LOGGER.debug("Prevented attack on summoned entity: {}", target.getUUID());
-                    return;
-                }
-                LastAttackerStorage.setLastAttacker(target, (Player) event.getEntity());
-                LOGGER.debug("Set last attacker for entity: {} - {}", target, event.getEntity().getUUID());
-            }
+    public static void onServerChatEvent(ServerChatEvent event) {
+        LOGGER.info("Executing ServerChatEvent for {}", event.getUsername());
+    }
+
+    @SubscribeEvent
+    public static void onAttachCapabilitiesEvent(AttachCapabilitiesEvent<Entity> event) {
+        if (event.getObject() instanceof Player) {
+            LOGGER.info("Executing AttachCapabilitiesEvent for Player");
         }
     }
 
     @SubscribeEvent
-    public static void onLivingHurt(LivingHurtEvent event) {
-        if (event.getSource().getEntity() instanceof Player && event.getEntity() instanceof Mob) {
-            Player player = (Player) event.getSource().getEntity();
-            Mob target = (Mob) event.getEntity();
-            PlayerTargetStorage.setPlayerTarget(player.getUUID(), target);
+    public static void onRegisterCapabilitiesEvent(RegisterCapabilitiesEvent event) {
+        LOGGER.info("Executing RegisterCapabilitiesEvent");
+    }
 
-            // Capture NBT data *before* death
-            CompoundTag entityData = new CompoundTag();
-            target.save(entityData);
-            preDeathEntityData.put(target.getUUID(), entityData);
+    @SubscribeEvent
+    public static void onItemAttributeModifierEvent(ItemAttributeModifierEvent event) {
+        ItemStack stack = event.getItemStack();
+        UUID attributeUUID = UUID.fromString("91AEc26e-37CB-469F-A1D5-F01F0DF2586E");
 
-            LOGGER.debug("Set player target: {} - {}", player.getUUID(), target);
-            LOGGER.debug("Stored pre-death data for entity: {}", target.getUUID());
+        if (stack.getDisplayName().getString().contains("Solo Leveling Sword")) {
+            AttributeModifier modifier = new AttributeModifier(attributeUUID, "solo_leveling_damage", 5, AttributeModifier.Operation.ADDITION);
+            event.addModifier(ModAttributes.DAMAGE.get(), modifier);
         }
     }
 
     @SubscribeEvent
-    public static void onPlayerLoggedIn(PlayerEvent.PlayerLoggedInEvent event) {
-        if (event.getEntity() instanceof ServerPlayer) {
-            ServerPlayer player = (ServerPlayer) event.getEntity();
-            if (EntityStorage.hasEntities(player.getUUID())) {
-                EntityStorage.spawnStoredEntities(player);
-                LOGGER.debug("Spawned stored entities for player: {}", player.getUUID());
+    public static void onEntityAttributeCreationEvent(EntityAttributeCreationEvent event) {
+        LOGGER.info("Executing EntityAttributeCreationEvent");
+    }
+
+    @SubscribeEvent
+    public static void onLivingHurtEvent(LivingHurtEvent event) {
+        DamageSource source = event.getSource();
+        Entity directEntity = source.getDirectEntity();
+
+        if (directEntity instanceof Player player) {
+            LivingEntity entity = event.getEntity();
+            PlayerTargetStorage.setPlayerTarget(player.getUUID(), entity);
+            LOGGER.debug("Set player target: {} - {}", player.getUUID(), entity);
+        }
+    }
+
+    @SubscribeEvent
+    public static void onLivingDeathEvent(LivingDeathEvent event) {
+        LivingEntity entity = event.getEntity();
+        DamageSource source = event.getSource();
+        Entity killer = source.getEntity();
+        ResourceLocation entityResourceLocation = entity.getType().getDefaultLootTable();
+
+        if (killer instanceof Player player) {
+            UUID playerUUID = player.getUUID();
+            ServerLevel serverLevel = player.getLevel() instanceof ServerLevel ? (ServerLevel) player.getLevel() : null;
+
+            if (serverLevel != null && ConfigManager.isNormalEnemy(entityResourceLocation)) {
+                CompoundTag entityData = new CompoundTag();
+                entity.save(entityData);
+                UUID entityUUID = entity.getUUID();
+                preDeathData.put(entityUUID, entityData);
+                LOGGER.debug("Stored pre-death data for entity: {}", entityUUID);
+
+                // Print the contents of the entityData tag
+                LOGGER.debug("Entity data: {}", entityData);
+
+                EntityStorage.storeEntity(playerUUID, entity, entityData);
+                LOGGER.debug("Stored entity for player: {} - {}", playerUUID, entityResourceLocation);
+
+                preDeathData.remove(entityUUID);
+                LOGGER.debug("Removed pre-death data for entity: {}", entityUUID);
             } else {
-                LOGGER.debug("No stored entities for player: {}", player.getUUID());
-            }
-        }
-    }
-
-    @SubscribeEvent
-    public static void onPlayerLoggedOut(PlayerEvent.PlayerLoggedOutEvent event) {
-        if (event.getEntity() instanceof Player) {
-            Player player = (Player) event.getEntity();
-            EntityStorage.clearEntities(player.getUUID());
-            LOGGER.debug("Cleared entities for player: {}", player.getUUID());
-        }
-    }
-
-    @SubscribeEvent
-    public static void onLivingDeath(LivingDeathEvent event) {
-        if (!(event.getEntity() instanceof LivingEntity)) {
-            return;
-        }
-
-        LivingEntity entity = (LivingEntity) event.getEntity();
-        ResourceLocation entityId = ForgeRegistries.ENTITY_TYPES.getKey(entity.getType());
-
-        LOGGER.debug("Entity died: {}", entityId);
-
-        // Check if the entity was discarded (e.g., when clearing entities)
-        if (entity.isRemoved()) {
-            LOGGER.debug("Entity was discarded, not storing it again.");
-            return;
-        }
-
-        // Check if the death was caused by a player
-        DamageSource damageSource = event.getSource();
-        if (damageSource != null && damageSource.getEntity() instanceof Player) {
-
-            //Force execution on the server thread
-            if (!entity.level().isClientSide) {
-                ServerLevel serverLevel = (ServerLevel) entity.level();
-                MinecraftServer server = serverLevel.getServer();
-                server.execute(() -> {
-                    // Get the last attacker
-                    UUID lastAttackerUUID = LastAttackerStorage.getLastAttacker(entity);
-
-                    if (lastAttackerUUID != null) {
-                        // Retrieve stored NBT data
-                        CompoundTag entityData = preDeathEntityData.get(entity.getUUID());
-                        if (entityData != null) {
-                            LOGGER.debug("Retrieved pre-death data for entity: {}", entity.getUUID());
-                            EntityStorage.storeEntity(lastAttackerUUID, entity, entityData);
-                            LOGGER.debug("Stored entity for player: {} - {}", lastAttackerUUID, entityId);
-                            LastAttackerStorage.clearLastAttacker(entity);
-                            PlayerTargetStorage.clearPlayerTarget(lastAttackerUUID);
-
-                            // Remove from temporary storage
-                            preDeathEntityData.remove(entity.getUUID());
-                            LOGGER.debug("Removed pre-death data for entity: {}", entity.getUUID());
-                        } else {
-                            LOGGER.warn("No pre-death entity data found for {}", entityId);
-                        }
-                    } else {
-                        LOGGER.debug("Entity not killed by player, no last attacker found.");
-                    }
-                });
-            } else {
-                LOGGER.debug("Skipping on client side.");
+                LOGGER.warn("Entity {} is not a valid target for player {}.", entity, player);
             }
         } else {
-            LOGGER.debug("Entity died from non-player source: {}", damageSource);
+            LOGGER.debug("Entity died from non-player source: {}", source);
         }
+
+        if (entity instanceof Player player) {
+            UUID playerUUID = player.getUUID();
+            EntityStorage.clearEntities(playerUUID);
+            LOGGER.debug("Cleared entities for player: {}", playerUUID);
+        }
+    }
+
+    @SubscribeEvent
+    public static void onPlayerJoinEvent(PlayerEvent.PlayerLoggedInEvent event) {
+        Player player = event.getEntity();
+        UUID playerUUID = player.getUUID();
+
+        LOGGER.debug("No stored entities for player: {}", playerUUID);
+    }
+
+    @SubscribeEvent
+    public static void onPlayerLeaveEvent(PlayerEvent.PlayerLoggedOutEvent event) {
+        Player player = event.getEntity();
     }
 }
